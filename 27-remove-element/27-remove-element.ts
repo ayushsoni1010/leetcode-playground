409bf33c18cf95aaea35d9e26922c16947dd4979
Ayush Soni <76895975+ayushsoni1010@users.noreply.github.com>
2022-07-30 17:19:07 +0530

    Time: 112 ms (32.62%), Space: 44.1 MB (41.12%) - LeetHub

---
+++ 27-remove-element/27-remove-element.ts
@@ -0,0 +1,9 @@
+function removeElement(nums: number[], val: number): number {
+    for(let i:number = 0; i<nums.length;i++){
+        if(nums[i] === val){
+            nums.splice(i,1);
+            i--;
+        }
+    }
+    return nums.length;
+};
