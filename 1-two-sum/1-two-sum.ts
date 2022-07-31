0d3e53a03c1dbdb68526a4ba36a8a4a8ed082bb2
Ayush Soni <76895975+ayushsoni1010@users.noreply.github.com>
2022-08-01 00:19:04 +0530

    Time: 131 ms (49.98%), Space: 44.9 MB (55.90%) - LeetHub

---
+++ 1-two-sum/1-two-sum.ts
@@ -0,0 +1,9 @@
+function twoSum(nums: number[], target: number): number[] {
+    for(let i:number = 0; i < nums.length; i++){
+        for(let j:number = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i,j];
+            }
+        }
+    }
+};
