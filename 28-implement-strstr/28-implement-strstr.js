83f417d6521712caba095802a01ea84c8552319b
Ayush Soni <76895975+ayushsoni1010@users.noreply.github.com>
2022-08-07 21:58:17 +0530

    Time: 78 ms (69.56%), Space: 42.3 MB (24.68%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(needle.length === 0) return 0;
+    return haystack.indexOf(needle);
+};
