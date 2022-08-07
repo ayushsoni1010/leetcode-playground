596a2c78441df4e3d79b0ef36ec21efc740d7b42
Ayush Soni <76895975+ayushsoni1010@users.noreply.github.com>
2022-08-07 23:56:53 +0530

    Time: 2 ms (40.77%), Space: 2 MB (65.82%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.go
+++ 28-implement-strstr/28-implement-strstr.go
@@ -1,6 +1,11 @@
-class Solution {
-    public int strStr(String haystack, String needle) {
-        if(needle.length() == 0) return 0;
-        return haystack.indexOf(needle);
+func strStr(haystack string, needle string) int {
+    if len(needle) == 0 || haystack == needle {
+        return 0;
     }
+    for i := 0; i <= len(haystack) - len(needle); i++ {
+        if haystack[i:len(needle) + i] == needle {
+            return i;
+        }
+    }
+    return -1;
 }
