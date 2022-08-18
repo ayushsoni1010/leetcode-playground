8c0b382209e656444deeed00ec756cc12eefc286
Ayush Soni <76895975+ayushsoni1010@users.noreply.github.com>
2022-08-19 00:23:09 +0530

    Time: 119 ms (45.24%), Space: 44.6 MB (69.05%) - LeetHub

--- 342-power-of-four/342-power-of-four.ts
+++ 342-power-of-four/342-power-of-four.ts
@@ -1,5 +1,5 @@
 function isPowerOfFour(n: number): boolean {
     if(n === 0) return false;
-    let num = Math.log(n) / Math.log(4);
-    return num === Math.floor(num)
+    let num:number = Math.log(n) / Math.log(4);
+    return num === Math.floor(num);
 };
