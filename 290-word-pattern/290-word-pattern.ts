6e5f91cd975982b73de420318734976e6f37f200
Ayush Soni <76895975+ayushsoni1010@users.noreply.github.com>
2022-09-02 23:57:54 +0530

    Time: 120 ms (15.18%), Space: 42.3 MB (95.54%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.ts
@@ -0,0 +1,21 @@
+function wordPattern(pattern: string, s: string): boolean {
+  const stringAsArray = s.split(" ");
+  if(pattern.length !== stringAsArray.length) return false;
+  const stringToPatternMap = {};
+  const patternToStringMap = {};
+  
+  for(let i = 0 ; i < pattern.length ; i++){
+    const patternChar = pattern[i];
+    const str = stringAsArray[i];
+    
+    if(!(str in stringToPatternMap) && patternChar in patternToStringMap)
+      return false;
+    
+    if(str in stringToPatternMap && stringToPatternMap[str] !== patternChar)
+      return false;
+    
+    stringToPatternMap[str] = patternChar;
+    patternToStringMap[patternChar] = str;
+  }
+  return true;
+};
